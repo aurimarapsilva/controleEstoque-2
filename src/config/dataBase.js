6c9b527d5a01ef057bb0a1a8523583253a758c8e
Ivian do Carmo <ivianqwr@gmail.com>
2021-11-12 07:21:47 -0300

    Atualizar conexão com o banco

--- src/config/dataBase.js
+++ src/config/dataBase.js
@@ -1,9 +1,11 @@
+const {host, username, password, database} = require('../../.env');
+
 module.exports = {
     dialect: 'mysql',
-    host: '10.0.0.25',
-    username: 'ti',
-    password: 'cTi_@niger',
-    database: 'controleEstoque',
+    host,
+    username,
+    password,
+    database,
     define: {
         timestamps: true,
         underscored: true,
